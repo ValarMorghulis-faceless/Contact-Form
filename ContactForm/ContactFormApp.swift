88334931e18a224e5675cdaae9b235ce2f5c539d
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-03 04:35:21 +0400

    Contact Form Project

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Giorgi Samkharadze on 30.08.22.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
